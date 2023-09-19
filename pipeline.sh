1a1bb45d3acfeba2ee7a03992cfda908e9264a89
tirumalareddysanampudi <45302057+tirumalareddysanampudi@users.noreply.github.com>
2023-09-19 13:40:55 +0530

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -7,14 +7,17 @@ pipeline {
                 git credentialsId: 'd1ea93fa-dbdf-4a89-a0bb-00c78b7fe433', url: 'https://github.com/tirumalareddysanampudi/maven_Project.git'
 
                 // Run Maven on a Unix agent.
-                sh "mvn -Dmaven.test.failure.ignore=true clean test"
+                sh "mvn -Dmaven.test.failure.ignore=true clean "
             }
             }
+        stage('test'){
+            steps{
+             sh "mvn -Dmaven.test.failure.ignore=true  test"
+        }
         stage('compile'){
             steps{
               //Run Maven Compil
               sh "mvn -Dmaven.test.failure.ignore=tru compile"
         }
-    }
     }
     }
